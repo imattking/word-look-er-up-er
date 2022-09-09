cf3e2d5606134fa8679c61cb76c5167109c2a642
imattking <matt@imattking.com>
2022-09-09 01:53:33 -0400

    add support for multiple definitions displaying at once

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const button = document.querySelector('button');
 button.addEventListener('click', () => {
     document.querySelector('#syntax').innerHTML = '';
     document.querySelector('#pronunciation').innerHTML = 'pronunciation: ';
-    document.querySelector('#short').innerHTML = 'definition: ';
+    document.querySelector('ol').innerHTML = '';
 
     const word = document.querySelector('input').value;
     
@@ -14,11 +14,26 @@ button.addEventListener('click', () => {
         .then(res => res.json())
         .then(data => {
             console.log(data[0].fl);
+            console.log(data);
+            // repeat serach term in the DOM
             document.querySelector('#word').innerHTML = `${word}`;
+            // access array and place part of speech in the COM
             document.querySelector('#syntax').innerHTML = `<em>${data[0].fl}</em>`
+            // acces array and place pronunciation in the DOM
             document.querySelector('#pronunciation').innerHTML = `pronunciation: ${data[0].hwi.prs[0].mw}`
-            document.querySelector('#short').innerHTML = `definition: ${data[0].shortdef[0]}`;
+            // access array and iterate through all elements to pull definition
+            data[0].shortdef.forEach( e => {
+                // acesss elements to verify
+                console.log(e);
+                // create an li for each element
+                const li = document.createElement('li');
+                // add text to li
+                li.textContent = e;
+                // append each li to the ul
+                document.querySelector('#definition').appendChild(li);
+            });
         })
+        // error handling
         .catch(err => {
             console.log(`error is: ${err}`);
             document.querySelector('#word').innerHTML = `${word} not found`;
